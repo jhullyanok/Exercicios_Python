761d0c975d95c342e442206b45107892a54c26c1
Jhullyano Keviny <jhullyanok@gmail.com>
2025-05-14 00:15:53 -0400

    Update Ex006.py

--- Ex006.py
+++ Ex006.py
@@ -1,5 +1,3 @@
 #Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada
-a = int(input('Digite qualquer número: '))
-print('O dobro de {} é {}!'.format(a, a*2))
-print('O triplo de {} é {}!'.format(a, a*3))
-print('A raiz quadrada de {} vale {:.3f}!'.format(a, a**(1/2)))
+ = int(input('Digite um número: '))
+print('Analizando {}:\nO dobro é {}.\nO triplo é {}.\nE a raiz quadrada é {:.2f}.'.format(n, n*2, n*3,n ** (1/2)))
