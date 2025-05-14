e39dc033bdbcde8b342db4d3ea3b29b822a28168
Jhullyano Keviny <jhullyanok@gmail.com>
2025-05-14 00:15:20 -0400

    Update Ex007.py

--- Ex007.py
+++ Ex007.py
@@ -1,5 +1,8 @@
 #Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média
-a = float(input('Nota 1 do aluno: '))
-b = float(input ('Nota 2 do aluno: '))
-s = (a+b) /2
-print('A média entre {:.1f} e {:.1f} é {:.1f}!'.format(a, b, s))
+nome = str(input('Qual é o nome do aluno? '))
+nota1 = float(input('A primeira nota do aluno: '))
+nota2 = float(input('A segunda nota: '))
+nota3 = float(input('A terceira nota: '))
+nota4 = float(input('A quarta nota: '))
+media = (nota1 + nota2 + nota3 + nota4) / 4
+print ('\nCalculando as notas do aluno {},a sua média escolar é {:.1f} !\n'.format(nome, media))
