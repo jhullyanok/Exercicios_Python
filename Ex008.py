74df41a714ba0c28de0d67d9b2f9ea347ce635c1
Jhullyano Keviny <jhullyanok@gmail.com>
2025-05-14 00:54:10 -0400

    Update Ex008.py

--- Ex008.py
+++ Ex008.py
@@ -1,3 +1,9 @@
 #Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros
 m = float(input('Digite o metro desejado: '))
-print('O metro {} convertido é:\nEm centimetros é {:.0f}cm!\nEm milímetros é {:.0f}mm!\nEm quilômetro  é {}km!\nEm hectometro é {}hm\nEm decametros é {}dam!\nEm decimetros é {}dm!'.format(m , m*100 , m *1000, m/1000, m/100, m/10, m*10))
+print('O metro {} convertido é: '.format(m))
+print('Em centimetros é {:.0f}cm! '.format(m *100))
+print('Em milímetros é {:.0f}mm! '.format(m *1000))
+print('Em quilômetro  é {}km! '.format(m /1000))
+print('Em hectometro é {}hm! '.format(m /100))
+print('Em decametros é {}dam! '.format(m /10))
+print('Em decimetros é {}dm! '.format(m *10))
